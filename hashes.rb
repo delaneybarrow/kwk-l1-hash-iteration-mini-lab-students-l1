82b48bb2808735c1d754de36de5539b5763292a3
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-06 21:43:34 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -1,1 +1,12 @@
 hash = {"cheese" => "stuff on pizza"}
+
+characters = ["monica", "Joey", "Phoebe"]
+characteristics = ["Neat Freak", "Jokester", "Free Spirit"]
+
+friends_characteristics = {}
+
+index = 0 
+characteristics.each do |name|
+  friends_characteristics[name] = characters
+  index += 1
+end
